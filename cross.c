a31747723b40604697aa52854b273eab0dadbd7d
Muhammad Zain <95617382+zyn10@users.noreply.github.com>
2022-08-20 09:16:44 +0500

    Update cross.c

--- cross.c
+++ cross.c
@@ -2,23 +2,25 @@
 using namespace std;
 int main()
 {
-	int Vector1[3], Vector2[3], DotProduct = 0;
-	cout << "Enter vector   no 1 in the form of i,j,k" ; cout << endl;
-	cout << "vector 1 is "; cout << endl;
-	for (int loop_1 = 0; loop_1 < 3; loop_1++)//loop for getting input for vector 1
+	int vector1[3], vector2[3], CrossProduct[3];
+	cout << "Enter vector   no 1 in the form of i,j,k"; cout << endl;
+	for (int loop_1 = 0; loop_1 < 3; loop_1++)//getting input for 1st variable
 	{
-		cin >> Vector1[loop_1];
+		cin >> vector1[loop_1];
 	}
-	cout << "Enter vector no 2 in the for of i,j,k"; cout << endl;
-	cout << "Vector 2 is "; cout << endl;
-	for (int loop_2 = 0; loop_2 < 3; loop_2++)//loop for geeting input for vector 2
+	cout << "Enter vector   no 1 in the form of i,j,k"; cout << endl;
+	for (int loop_2 = 0; loop_2 < 3; loop_2++)//geting input for 2nd vector
 	{
-		cin >> Vector2[loop_2];
+		cin >> vector2[loop_2];
 	}
+	CrossProduct[0] = (vector1[1] * vector2[2]) - (vector1[2] * vector2[1]);//formula
+	CrossProduct[1] = (vector1[2] * vector2[0]) - (vector1[0] * vector2[2]);//formula 
+	CrossProduct[2] = (vector1[0] * vector2[1]) - (vector1[1] * vector2[0]);//formula
+
+	cout << "The Cross Product of first and second vector"; cout << endl;
 	for (int loop_3 = 0; loop_3 < 3; loop_3++)
 	{
-		DotProduct = DotProduct + (Vector1[loop_3] * Vector2[loop_3]);//formulae
+		cout << CrossProduct[loop_3] << endl;
 	}
-	cout << "Dot product of first and second vector = " << DotProduct;
 	system("pause>0");
 }
